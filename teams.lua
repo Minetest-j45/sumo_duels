a57950288c474aec739dd40db824ba70a7084d58
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-15 22:07:21 +0000

    Update teams.lua

--- teams.lua
+++ teams.lua
@@ -1,5 +1,7 @@
 sumo_deuls.teams = {lobby = {}, waiting_arena_1 = {}, arena_1 = {}}
 
+local sumo_deuls.before = {}
+
 sumo_deuls.get_player_team = function(name)
 	for k, team in pairs(sumo_deuls.teams) do
 		for _, pname in ipairs(team) do
