f5850decb87eeee84a0de65a80986afcc90ea17d
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-15 22:05:02 +0000

    Update teams.lua

--- teams.lua
+++ teams.lua
@@ -1,1 +1,6 @@
+sumo_deuls.teams = {lobby = {}, waiting_arena_1 = {}, arena_1 = {}}
 
+minetest.register_on_joinplayer(function(player)
+    local pname = player:get_player_name() 
+    table.insert(sumo_deuls.teams[lobby], pname)
+end)
