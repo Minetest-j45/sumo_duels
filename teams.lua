cbb22d8a34fc3132f54fcf8638245a8e4dce2d94
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-15 22:06:36 +0000

    Update teams.lua

--- teams.lua
+++ teams.lua
@@ -1,5 +1,13 @@
 sumo_deuls.teams = {lobby = {}, waiting_arena_1 = {}, arena_1 = {}}
 
+sumo_deuls.get_player_team = function(name)
+	for k, team in pairs(sumo_deuls.teams) do
+		for _, pname in ipairs(team) do
+			if name == pname then return k end
+		end
+	end
+end
+
 minetest.register_on_joinplayer(function(player)
     local pname = player:get_player_name() 
     table.insert(sumo_deuls.teams[lobby], pname)
