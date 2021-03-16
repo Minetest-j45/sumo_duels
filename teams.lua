1289563fbe800ca77cffcc5796c5f2c4c22cb1d5
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-16 17:19:55 +0000

    Update teams.lua

--- teams.lua
+++ teams.lua
@@ -14,3 +14,15 @@ minetest.register_on_joinplayer(function(player)
     local pname = player:get_player_name() 
     table.insert(sumo_deuls.teams[lobby], pname)
 end)
+
+function sumo_duels.set_playing(player, arena_number)
+	--nothing yet
+end
+
+minetest.register_globalstep(function(dtime)
+	if #sumo_deuls.teams[arena_1] == 0 then --noone in arena1
+			if #sumo_deuls.teams[waiting_arena_1] >= 2 then
+				sumo_duels.set_playing(player, arena_number)
+			end
+	end
+end)
