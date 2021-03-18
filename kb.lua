2d58b718c37489e78393d0f3fd492b853c99b872
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-18 16:32:38 +0000

    make kb amount rely on time from last punch

--- kb.lua
+++ kb.lua
@@ -1,3 +1,9 @@
 minetest.calculate_knockback = function(player, hitter, time_from_last_punch, tool_capabilities, dir, distance, damage)
-	return 1
+	if time_from_last_punch >= 1.5 then
+		return 1.5
+	elseif time_from_last_punch <= 1 and time_from_last_punch >= 0.5 then
+		return 1
+	else
+		return 0.5
+	end
 end
