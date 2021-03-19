e75625e9c126ee58c44de1e43b12d00a13054326
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-19 16:42:50 +0000

    fix about 100 dumb typoes

--- teams.lua
+++ teams.lua
@@ -1,9 +1,9 @@
-sumo_deuls.teams = {lobby = {}, waiting_arena_1 = {}, arena_1 = {}}
+sumo_duels.teams = {lobby = {}, waiting_arena_1 = {}, arena_1 = {}}
 
 local lobby_pos = {x = 0, y = 0, z = 0}
 
-sumo_deuls.get_player_team = function(name)
-	for k, team in pairs(sumo_deuls.teams) do
+sumo_duels.get_player_team = function(name)
+	for k, team in pairs(sumo_duels.teams) do
 		for _, pname in ipairs(team) do
 			if name == pname then return k end
 		end
@@ -11,23 +11,23 @@ sumo_deuls.get_player_team = function(name)
 end
 
 local other_player
-sumo_deuls.get_other_team_player = function(name)
-	local current = sumo_deuls.get_player_team(name)
-	for _, player in pairs(sumo_deuls.teams[current]) do
+sumo_duels.get_other_team_player = function(name)
+	local current = sumo_duels.get_player_team(name)
+	for _, player in pairs(sumo_duels.teams[current]) do
 		local pname = player:get_player_name()
 		if not name == pname then other_player = pname end
 	end
 end
 
 sumo_duels.set_playing = function(pname, arena_number)
-	local current = sumo_deuls.get_player_team(pname)
+	local current = sumo_duels.get_player_team(pname)
 	table.remove(current, pname)
 	--set pos arena_1
 	table.insert(sumo_duels.teams["arena_" .. arena_number], pname)
 end
 
 sumo_duels.set_waiting = function(pname, arena_number)
-	local current = sumo_deuls.get_player_team(pname)
+	local current = sumo_duels.get_player_team(pname)
 	table.remove(current, pname)
 	local player = minetest.get_player_by_name(pname)
 	player:set_pos(lobby_pos)
@@ -35,7 +35,7 @@ sumo_duels.set_waiting = function(pname, arena_number)
 end
 
 sumo_duels.set_lobby = function(pname)
-	local current = sumo_deuls.get_player_team(pname)
+	local current = sumo_duels.get_player_team(pname)
 	table.remove(current, pname)
 	local player = minetest.get_player_by_name(pname)
 	player:set_pos(lobby_pos)
@@ -44,7 +44,7 @@ end
 
 minetest.register_on_joinplayer(function(player)
     local pname = player:get_player_name()
-    table.insert(sumo_deuls.teams["lobby"], pname)
+    table.insert(sumo_duels.teams["lobby"], pname)
 end)
 
 minetest.register_chatcommand("join", {
@@ -62,13 +62,12 @@ minetest.register_chatcommand("join", {
 })
 
 minetest.register_globalstep(function(dtime)
-	if #sumo_deuls.teams["arena_1"] == 0 then --noone in arena1
-		if #sumo_deuls.teams["waiting_arena_1"] >= 2 then
-			for name, sumo_duels.teams["waiting_arena_1"] = 1,2 do
-				sumo_duels.set_playing(name, "1")
-			end
+	if #sumo_duels.teams["arena_1"] == 0 then --noone in arena1
+		if #sumo_duels.teams["waiting_arena_1"] >= 2 then
+			sumo_duels.set_playing(sumo_duels.teams["waiting_arena_1"], "1") 
+			sumo_duels.set_playing(sumo_duels.teams["waiting_arena_1"], "1") 
 		end
-	elseif #sumo_deuls.teams["arena_1"] == 1 then
+	elseif #sumo_duels.teams["arena_1"] == 1 then
 		for _, name in ipairs(sumo_duels.teams["arena_1"]) do
 			minetest.chat_send_player(name, "Your opponent left. GG in a way. lol")
 			sumo_duels.set_lobby(name, "arena_1")
@@ -77,9 +76,9 @@ minetest.register_globalstep(function(dtime)
 end)
 
 minetest.register_on_dieplayer(function(player)
-	sumo_deuls.get_other_team_player(player:get_player_name())
+	sumo_duels.get_other_team_player(player:get_player_name())
 	minetest.chat_send_player(other_player, "Your opponent died! GG, you win")
-	for _, teamplayer in ipairs(sumo_duels.teams[sumo_deuls.get_player_team[player:get_player_name()]]) do
+	for _, teamplayer in ipairs(sumo_duels.teams[sumo_duels.get_player_team[player:get_player_name()]]) do
 		sumo_duels.set_lobby(teamplayer:get_player_name())
 	end
 end)
