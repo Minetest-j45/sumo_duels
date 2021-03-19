46e72cca9fe1966efe1871acdfb4e947a2f75e42
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-19 16:29:59 +0000

    Update teams.lua

--- teams.lua
+++ teams.lua
@@ -23,7 +23,7 @@ sumo_duels.set_playing = function(pname, arena_number)
 	local current = sumo_deuls.get_player_team(pname)
 	table.remove(current, pname)
 	--set pos arena_1
-	table.insert(sumo_duels[arena_ .. arena_number], pname)
+	table.insert(sumo_duels.teams["arena_" .. arena_number], pname)
 end
 
 sumo_duels.set_waiting = function(pname, arena_number)
@@ -31,7 +31,7 @@ sumo_duels.set_waiting = function(pname, arena_number)
 	table.remove(current, pname)
 	local player = minetest.get_player_by_name(pname)
 	player:set_pos(lobby_pos)
-	table.insert(sumo_duels[waiting_arena_ .. arena_number], pname)
+	table.insert(sumo_duels.teams["waiting_arena_" .. arena_number], pname)
 end
 
 sumo_duels.set_lobby = function(pname)
@@ -39,12 +39,12 @@ sumo_duels.set_lobby = function(pname)
 	table.remove(current, pname)
 	local player = minetest.get_player_by_name(pname)
 	player:set_pos(lobby_pos)
-	table.insert(sumo_duels[lobby], pname)
+	table.insert(sumo_duels.teams["lobby"], pname)
 end
 
 minetest.register_on_joinplayer(function(player)
     local pname = player:get_player_name()
-    table.insert(sumo_deuls.teams[lobby], pname)
+    table.insert(sumo_deuls.teams["lobby"], pname)
 end)
 
 minetest.register_chatcommand("join", {
@@ -62,16 +62,16 @@ minetest.register_chatcommand("join", {
 })
 
 minetest.register_globalstep(function(dtime)
-	if #sumo_deuls.teams[arena_1] == 0 then --noone in arena1
-		if #sumo_deuls.teams[waiting_arena_1] >= 2 then
-			for name, sumo_duels.teams[waiting_arena_1] = 1,2 do
+	if #sumo_deuls.teams["arena_1"] == 0 then --noone in arena1
+		if #sumo_deuls.teams["waiting_arena_1"] >= 2 then
+			for name, sumo_duels.teams["waiting_arena_1"] = 1,2 do
 				sumo_duels.set_playing(name, "1")
 			end
 		end
-	elseif #sumo_deuls.teams[arena_1] == 1 then
-		for _, name in ipairs(sumo_duels.teams[arena_1]) do
+	elseif #sumo_deuls.teams["arena_1"] == 1 then
+		for _, name in ipairs(sumo_duels.teams["arena_1"]) do
 			minetest.chat_send_player(name, "Your opponent left. GG in a way. lol")
-			sumo_duels.set_lobby(name, arena_1)
+			sumo_duels.set_lobby(name, "arena_1")
 		end
 	end
 end)
