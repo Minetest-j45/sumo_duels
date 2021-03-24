cb52adad1e231fcfcc528d216787b0bb1d758f8d
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-24 17:22:13 +0000

    Update messages.lua

--- messages.lua
+++ messages.lua
@@ -1,1 +1,12 @@
+sumo_duels.messages = {"You can join the waiting list for an arena using /join <number from 1 to 3>", "The longer you wait to hit your opponent, the more knockback you do"}
 
+local timer = 0
+minetest.register_globalstep(function(dtime)
+	timer = timer + dtime;
+	if timer >= 5 then
+		-- Send message to all players every 5 seconds
+    local random = math.random(1, #sumo_duels.messages)
+		minetest.chat_send_all(minetest.colorize("#999997", sumo_duels.messages[random]))
+		timer = 0
+	end
+end)
