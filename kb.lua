e8ed720de22a686e6f8001e39db187912c1c9e33
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-25 22:27:25 +0000

    Update kb.lua

--- kb.lua
+++ kb.lua
@@ -1,4 +1,5 @@
 minetest.calculate_knockback = function(player, hitter, time_from_last_punch, tool_capabilities, dir, distance, damage)
+    player:set_hp(20)
     if not time_from_last_punch or time_from_last_punch >= 1.5 then
         return 1.5
     elseif time_from_last_punch <= 1 and time_from_last_punch >= 0.5 then
